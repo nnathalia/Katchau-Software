f7da047a9f127b6f8d2605338d156d05ab993030
nathalia <nmarianolopes@gmail.com>
2024-07-31 21:58:35 -0400

    update templates

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,9 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('components/layout/index')
+router.on('/shop').render('pages/shop').as('shop')
+router.on('/about').render('pages/about').as('about')
+router.on('/contact').render('pages/contact').as('contact')
+router.on('/cart').render('pages/cart').as('cart')
+router.on('/checkout').render('pages/checkout').as('checkout')
