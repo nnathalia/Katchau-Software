32fe3bc7733b21f7672639ee9ae1432e73d05f88
nathalia <nmarianolopes@gmail.com>
2024-08-01 16:24:04 -0400

    update

--- start/routes.ts
+++ start/routes.ts
@@ -10,8 +10,9 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('components/layout/index').as('index')
-router.on('/shop').render('pages/shop').as('shop')
 router.on('/about').render('pages/about').as('about')
 router.on('/contact').render('pages/contact').as('contact')
+router.on('/shop').render('pages/shop').as('shop')
+router.on('/singleproduct').render('pages/singleproduct').as('singleproduct')
 router.on('/cart').render('pages/cart').as('cart')
 router.on('/checkout').render('pages/checkout').as('checkout')
